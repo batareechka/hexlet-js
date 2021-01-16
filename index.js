84072aab40208ffa4efa64f325da4be51cb5e8f4
Anastasiia Batarei <batarey.work@gmail.com>
2021-01-16 20:05:22 +0300

    Task 7: add eslint

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-import _ from "lodash";
+import _ from 'lodash';
 
-console.log("Hello, Hexlet!");
-console.log(_.last(["one", "two"]));
+console.log('Hello, Hexlet!');
+console.log(_.last(['one', 'two']));
+
+const findSum = (a, b) => {
+  const c = a + b; return c;
+};
+
+findSum(5, 7);
